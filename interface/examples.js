00f035653b49a26b8b40f9e98e4ef35f2bb0fa3f
Dave Wilding <git@dpw.me>
2014-04-27 10:40:28 +0100

    Added several examples.

--- interface/examples.js
+++ interface/examples.js
@@ -5,6 +5,54 @@ storing the first defaultLength terms of each sequence we need only call
 Sequence.identify() when absolutely necessary.
 */
 var knownDeals = {
+	// repeated 0s
+	'0,0,0,0,0,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '0*0') {
+			return knownSequence('A000004');
+		}
+	},
+	// characteristic sequences
+	'1,0,0,0,0,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '0') {
+			return knownSequence('A000007');
+		}
+	},
+	'0,1,0,0,0,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '1') {
+			return knownSequence('A063524');
+		}
+	},
+	'0,0,1,0,0,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '2') {
+			return knownSequence('A185012');
+		}
+	},
+	'0,0,0,1,0,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '3') {
+			return knownSequence('A185013');
+		}
+	},
+	'0,0,0,0,1,0,0,0,0,0,0,0': function (identity) {
+		if (identity == '4') {
+			return knownSequence('A185014');
+		}
+	},
+	'0,0,0,0,0,1,0,0,0,0,0,0': function (identity) {
+		if (identity == '5') {
+			return knownSequence('A185015');
+		}
+	},
+	'0,0,0,0,0,0,1,0,0,0,0,0': function (identity) {
+		if (identity == '6') {
+			return knownSequence('A185016');
+		}
+	},
+	'0,0,0,0,0,0,0,1,0,0,0,0': function (identity) {
+		if (identity == '7') {
+			return knownSequence('A185017');
+		}
+	},
+	// Fibonacci numbers
 	'1,1,2,3,5,8,13,21,34,55,89,144': function (identity) {
 		if (identity == '(1or2)^?') {
 			return '<span class="sample">(1 or 2)^?</span> means &quot;deal 1\
@@ -18,6 +66,12 @@ var knownDeals = {
 			example</a>';
 		}
 	},
+	'0,1,1,2,3,5,8,13,21,34,55,89': function (identity) {
+		if (identity == '(1or2)^?1') {
+			return knownSequence('A000045');
+		}
+	},
+	// 6 choose n
 	'1,6,15,20,15,6,1,0,0,0,0,0': function (identity) {
 		if (identity == '0or15*2or15*4or20*3or6or6*1or6*5') {
 			return '<span class="sample">(0 or 1)^6</span> means &quot;deal 0\
@@ -28,18 +82,26 @@ var knownDeals = {
 			another example</a>';
 		}
 	},
+	// repeated 1s
+	'1,1,1,1,1,1,1,1,1,1,1,1': function (identity) {
+		if (identity == '(1)^?') {
+			return knownSequence('A000012');
+		}
+	},
 	'0,1,1,1,1,1,1,1,1,1,1,1': function (identity) {
 		if (identity == '(1)^?1') {
 			return '<span class="sample">1...</span> means &quot;deal 1 card\
 			then deal as many cards as you like&quot;. If the deck contains 0\
 			cards then it is not possible to carry out this deal, but otherwise\
 			there is exactly 1 way you can use this deal to empty a deck of\
-			<b>n</b> &ge; 1 cards.';
+			<b>n</b> &ge; 1 cards.<br><a href="#2%20*%20...1...">Show me\
+			another example</a>';
 		}
 	},
-	'1,1,1,1,1,1,1,1,1,1,1,1': function (identity) {
-		if (identity == '(1)^?') {
-			return knownSequence('A000012');
+	// natural numbers
+	'1,2,3,4,5,6,7,8,9,10,11,12': function (identity) {
+		if (identity == '(1)^?(1)^?') {
+			return knownSequence('A000027');
 		}
 	},
 	'0,1,2,3,4,5,6,7,8,9,10,11': function (identity) {
@@ -47,16 +109,13 @@ var knownDeals = {
 			return knownSequence('A001477');
 		}
 	},
-	'1,2,3,4,5,6,7,8,9,10,11,12': function (identity) {
-		if (identity == '(1)^?(1)^?') {
-			return knownSequence('A000027');
+	// even numbers
+	'0,2,4,6,8,10,12,14,16,18,20,22': function (identity) {
+		if (identity == '2*(1)^?(1)^?1') {
+			return knownSequence('A005843');
 		}
 	},
-	'0,1,1,2,3,5,8,13,21,34,55,89': function (identity) {
-		if (identity == '(1or2)^?1') {
-			return knownSequence('A000045');
-		}
-	},
+	// powers of 2
 	'1,2,4,8,16,32,64,128,256,512,1024,2048': function (identity) {
 		if (identity == '(2*1)^?') {
 			return knownSequence('A000079');
